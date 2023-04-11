3e1ad7f599b8827b96b01db056d596686905354d
eduardonk9999 <thiago.novak7@gmail.com>
2023-04-11 14:41:21 -0300

    feat: insert

--- scripts.sql
+++ scripts.sql
@@ -1,10 +1,10 @@
 CREATE TABLE pessoas (
-nome varchar(30) NOT NULL,
-nascimento date,
-sexo enum('M', 'F'),
-peso decimal(5,2),
-altura decimal(3, 2),
-nacionalidade varchar(20) default 'Brasil'
+  nome varchar(30) NOT NULL,
+  nascimento date,
+  sexo enum('M', 'F'),
+  peso decimal(5,2),
+  altura decimal(3, 2),
+  nacionalidade varchar(20) default 'Brasil'
 
 ) DEFAULT CHARSET = utf8;
 
@@ -12,9 +12,16 @@ nacionalidade varchar(20) default 'Brasil'
 
 describe pessoas;
 
-//apagar
+-- apagar
 drop databasde nameBanco;
 
-// criar 
+-- criar 
 create database nameBanco default character set utf8
 default collate utf8_general_ci;
+
+-- inserindo
+INSERT INTO nomeTable (id, none, nascimento, sexo, peso, nacionalidade)
+VALUES
+('1', 'Joao', '1986-01-02', 'M', '78.5', '1.83', 'Brasil');
+
+-- alterando
